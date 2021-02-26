ec4db50e802b3b91ae3da2c1b152695cf2797c91
ANDY <h880702520@gmail.com>
2021-02-26 10:34:58 +0800

    Update sanctum.php

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,11 +15,11 @@ return [
 
     'stateful' => explode(',', env(
         'SANCTUM_STATEFUL_DOMAINS',
-        //'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1' // default
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1' // default
 
         //'localhost, localhost:3000, localhost:3306, 127.0.0.1, 127.0.0.1:8000,::1' // new
 
-        'localhost, localhost:3306, 127.0.0.1, 127.0.0.1:8000,::1' // new
+        // 'localhost, localhost:3306, 127.0.0.1, 127.0.0.1:8000,::1' // new
     )),
 
     /*
