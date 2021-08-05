75cdb89b7f504d993f8a097b2c9a400dcd9b0ea4
Andy-Ho8872 <h880702520@gmail.com>
2021-08-05 18:33:58 +0800

    move controller logic to OrderService

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,128 +3,36 @@
 namespace App\Http\Controllers;
 
 //* Facades
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-
-//* Models
-use App\Models\Cart;
-use App\Models\Order;
-// use App\Models\OrderProduct;
-use App\Models\User;
-
 //* Requests
-use App\Http\Requests\MakeOrderRequest;
-
-//* Notification
-use Illuminate\Support\Facades\Notification;
-use App\Notifications\OrderCreated;
+use App\Http\Requests\OrderRequest;
+//* Service 
+use App\Services\OrderService;
 
 class OrderController extends Controller
-{
-    private $user_id, $carts, $user_orders;
-
-    public function __construct()
-    //* 常用變數
-    {
-        $this->middleware(function ($request, $next) {
-            //* 取得已經登入的使用者
-            $this->user_id = Auth::id();
-            $this->user = User::find($this->user_id);
-            //* 使用者的購物車
-            $this->carts = Cart::where('user_id', $this->user_id);
-            //* 使用者的訂單
-            $this->user_orders = Order::query()
-                ->with(['items'])
-                ->withCount('items') //* 該筆訂單所購買的商品個數
-                ->where('user_id', $this->user_id);
-
-            return $next($request);
-        });
-    }
-
-//* 推送通知
-    public function notifyWhenOrderCreated($order)
-    {
-        $user = User::find($this->user_id);
-        $details = [
-            'order_id' => $order->id,
-            'payment_id' => $order->payment_id,
-        ];
-        Notification::send($user, new OrderCreated($details));
-    }
-    
+{ 
 //* 建立訂單
-    public function createOrder(MakeOrderRequest $request)
+    public function createOrder(OrderRequest $request, OrderService $service)
     {
-        //* 確認購物車內是否有商品
-        if ($this->carts->exists()) {
-            //? 建立訂單
-            $order = Order::create([
-                'user_id' => $this->user_id,
-                'payment_id' => $request->payment_id,
-                'address' => $request->address,
-            ]);
-            //? 將資料寫入到 order_products 表格裡面做紀錄
-            $carts = $this->carts->get();
-            $items = [];  //? 宣告為陣列
-            foreach($carts as $cart) {
-                $items[] = [
-                    'order_id' => $order->id,
-                    'product_id' => $cart->product_id,
-                    'product_quantity' => $cart->product_quantity
-                ];
-            }
-            DB::table('order_product')->insert($items);
-            // 回傳訊息
-            $msg = "訂單建立成功";
-            //* 訂單建立後刪除購物車內的商品
-            $this->carts->delete();   
-            //* 推送通知
-            $this->notifyWhenOrderCreated($order);
-        } else {
-            $msg = "購物車內沒有商品，訂單建立失敗。";
-            return response()->json(['msg' => $msg], 400);
-        }
-        return response()->json(['msg' => $msg], 201);
+        return $service->createOrderAndSendNotification($request);
     }
 
 //* 撈取訂單
     //? 所有訂單
-    public function getAllOrders()
+    public function getAllOrders(OrderService $service)
     {
-        $orders = $this->user_orders->get()
-            ->append('sumSubtotal');
-
-        return response()->json(['orders' => $orders]);
+        return $service->getUserOrders();
     }
     //? 單筆訂單
-    public function getSingleOrder($order_id)
+    public function getSingleOrder(OrderService $service, $order_id)
     {
-        $order = $this->user_orders->findOrFail($order_id)
-            ->append('sumSubtotal');
-
-        return response()->json(['order' => $order]);
+        return $service->getUserSingleOrder($order_id);
     }
 
 //* 刪除訂單
-    public function deleteOrder($order_id)
+    public function deleteOrder(OrderService $service, $order_id)
     {
-        $order = Order::query()
-        ->where('user_id', $this->user_id)
-        ->where('id', $order_id);
-
-        //* 確認該筆訂單是否存在
-        if ($order->exists()) {
-            //? 刪除該筆訂單
-            $order->delete();
-            // 提示訊息
-            $msg = "您刪除了一筆訂單，訂單編號為${order_id}";
-        } else {
-            $msg = "該筆訂單不存在，操作失敗。";
-            return response()->json(['msg' => $msg], 400);
-        }
-
-        return response()->json(['msg' => $msg], 201);
+        return $service->deleteSingleOrder($order_id);
     }
 
 //* 前端表單資料
