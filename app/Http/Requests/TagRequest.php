a9f063e4240afa946594bf0602d84080ac900c87
Andy-Ho8872 <h880702520@gmail.com>
2021-08-07 22:41:48 +0800

    Create TagRequest.php

---
+++ app/Http/Requests/TagRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // return false; //! default
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|unique:tags|max:255|min:1'
+        ];
+    }
+}
