9889b0e232a8bef6884aee941a204acfd6b95372
Andy-Ho8872 <h880702520@gmail.com>
2021-08-25 18:50:34 +0800

    create user registered notification

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserRegistered extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    private $details;
+
+    public function __construct($details)
+    {
+        $this->details = $details;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        // return ['mail']; //! default
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+
+    // public function toArray($notifiable)
+    // {
+    //     return [
+    //         //
+    //     ];
+    // }
+
+    //* new
+    public function toDatabase($notifiable)
+    {
+        return [
+            'title' => $this->details['title'],
+            'avatar_url' => $this->details['avatar_url'],
+            'body' => $this->details['body'],
+            'created_at' => $this->details['created_at']
+        ];
+    }
+}
