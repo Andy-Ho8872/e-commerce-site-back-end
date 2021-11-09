4d2522a9bf99549ab0ee1ac4ff1fcba0d96694a5
Andy-Ho8872 <h880702520@gmail.com>
2021-11-09 13:23:39 +0800

    add migrations

---
+++ database/migrations/2021_10_09_100731_create_variations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVariationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->nullable()->constrained('products')
+            ->onUpdate('cascade')->onDelete('cascade');
+            $table->string('title')->nullable(); // 產品的規格，例如: 尺寸、顏色。
+            $table->json('options')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variations');
+    }
+}
